8b5779715fed46637d0185a229d141455f793c4c
Krishna Narkhede <krishna.narkhede23@vit.edu>
2024-10-21 23:25:31 +0530

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config(); 
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 // Middleware to parse incoming form data
 app.use(bodyParser.urlencoded({ extended: true }));
